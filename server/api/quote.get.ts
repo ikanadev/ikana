dac760c9182f95348335650aa919432dd7709006
Kevin Vargas <vargaskevv@gmail.com>
2024-08-12 21:10:00 -0400

    comment

--- server/api/quote.get.ts
+++ server/api/quote.get.ts
@@ -1,7 +1,7 @@
 import { quotes } from "@/server/utils/quotes";
 
 export default defineEventHandler(async (event) => {
-  await new Promise((res) => setTimeout(res, 1000));
+  // await new Promise((res) => setTimeout(res, 1000));
   const randomIndex = Math.floor(Math.random() * quotes.length);
   const quote = quotes[randomIndex];
   return quote;
